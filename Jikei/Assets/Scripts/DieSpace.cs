1ea9af3f1e72c54ad5770d75955e4b080c461d4c
ta1jin <qweax98@gmail.com>
2020-11-28 13:48:30 +0900

    Add dead and pause menu

---
+++ Jikei/Assets/Scripts/DieSpace.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DieSpace : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player"){
+            if (GameIsPaused)
+            {
+                Restart();
+             }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Restart()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Debug.Log("Load");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+    }
+    
+}
