b2d153fb9d982c241b7fd95f27c08418bf2c76db
erenije <scarecrovv228@yandex.ru>
2020-12-25 23:42:31 +0900

    add circleplatform

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		coll.transform.parent = transform;
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		coll.transform.parent = null;
+	}
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -82,4 +82,15 @@ public class PlayerMove : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
+    /*void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.name.Equals("tiles"))
+        coll.transform.parent = transform;
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if (coll.gameObject.name.Equals("tiles"))
+            coll.transform.parent = null;
+    }*/
 }
